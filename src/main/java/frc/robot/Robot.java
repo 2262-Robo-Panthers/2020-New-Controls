8726886f8c3c3e4b9a17cdd8541eeb51ff130586
Tejas Balcha <tejasbalcha@gmail.com>
2020-03-04 17:42:06 -0500

    Finished the Autonomus (for now)

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -98,9 +98,12 @@ public class Robot extends TimedRobot {
 	double flywheelMinSpeed = 0;
     boolean logiPOVWasDown = false;
 	boolean logiPOVUpWasPressed = false;
+	double motorRotation;
+	double gearboxRotation;
 	final double lowGearRatio = 20.8;
 	final double turnRadius = 11 * Math.PI;
-	final double driveEncoderPerRotation = 2048;
+	final int driveEncoderPerRotation = 2048;
+	final int distancePerWheelRotation = 6;
 
 
 	/*
@@ -204,12 +207,22 @@ public class Robot extends TimedRobot {
 			if (inPosition && autoTimer.get() < 5.0 && flywheel.getEncoder().getVelocity() > 1800) ConveyorGo();
 			else ConveyorStop();
 		}
-		if (autoTimer.get() >= 5) {
+		if (autoTimer.get() >= 7 && autoTimer.get() < 9) {
 			double motorRotation = fr.getSensorCollection().getIntegratedSensorPosition() / driveEncoderPerRotation;
-			double gearboxRotation = motorRotation / lowGearRatio;
+			gearboxRotation = motorRotation / lowGearRatio;
 			if (gearboxRotation < turnRadius/2) {
-				drive.arcadeDrive(0, 0.5);
+				drive.arcadeDrive(0, 0.6);
 			}
+		}
+		if (autoTimer.get() > 9 && autoTimer.get() <= 11) {
+			double distanceTraveled = gearboxRotation * distancePerWheelRotation;
+			if (distanceTraveled < 200) {
+				drive.arcadeDrive(-0.75, 0);
+			}
+		}
+		if (autoTimer.get() > 11) {
+			drive.arcadeDrive(0, 0);
+			stopper.set(Value.kReverse);
 		}
 
 		// if (autoTimer.get() < 3.0) flywheelSetpoint = 0;
